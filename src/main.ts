e1bc5cb419250e4574eb30732b923a44ec18cf03
Jessica Esteves <jessicanayaraesteves@gmail.com>
2025-06-04 14:01:09 -0300

    Adicionado cors e rota de exemplo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita CORS para o Angular (localhost:4200)
+  app.enableCors({
+    origin: 'http://localhost:4200', // ou origin: '*' para todos (não recomendado em prod)
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
